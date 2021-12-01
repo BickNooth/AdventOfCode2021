d804604bebecd7cbb85d19c1d504d7f22fe85d33
Nick Booth <nick-booth@live.com>
2021-12-01 10:07:05 +0000

    Day 1 Part 2

--- advent21/Day1/Day1.cs
+++ advent21/Day1/Day1.cs
@@ -4,12 +4,12 @@ public class Day1 : DayBase
 {
     public void Run()
     {
-        //var input = GetNumericalInput(".\\Day1\\testinput.txt"); //Test Input
         var input = GetNumericalInput(".\\Day1\\puzzleinput.txt"); //Puzzle Input
-        ProcessInput(input);
+        //ProcessInputIndividual(input); // Part 1 
+        ProcessInputWithThreeMeasurementSpan(input);
     }
 
-    private void ProcessInput(IEnumerable<int> input)
+    private void ProcessInputIndividual(IEnumerable<int> input)
     {
         var depths = input.ToList();
         int increasedFromPreviousMeasurement = 0;
@@ -21,6 +21,37 @@ public class Day1 : DayBase
                 increasedFromPreviousMeasurement++;
                 continue;
             } 
+            decreasedFromPreviousMeasurement++;
+        }
+
+        Console.WriteLine($"Increased: {increasedFromPreviousMeasurement}");
+        Console.WriteLine($"Decreased: {decreasedFromPreviousMeasurement}");
+        Console.ReadKey();
+    }
+
+    private void ProcessInputWithThreeMeasurementSpan(IEnumerable<int> input)
+    {
+        var depths = input.ToList();
+        int increasedFromPreviousMeasurement = 0;
+        int decreasedFromPreviousMeasurement = 0;
+
+
+        for (int i = 1; i < depths.Count(); i++)
+        {
+            int currentDepth = 
+                depths.ElementAtOrDefault(i) + 
+                depths.ElementAtOrDefault(i + 1) + 
+                depths.ElementAtOrDefault(i + 2);
+            int previousDepth = 
+                depths.ElementAtOrDefault(i-1) + 
+                depths.ElementAtOrDefault(i) + 
+                depths.ElementAtOrDefault(i + 1);
+
+            if (currentDepth > previousDepth)
+            {
+                increasedFromPreviousMeasurement++;
+                continue;
+            }
             decreasedFromPreviousMeasurement++;
         }
 
