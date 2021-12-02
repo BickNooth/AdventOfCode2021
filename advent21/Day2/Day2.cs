23fd82e79e351bb0610a6fd99482a82f5ed92f99
Nick Booth <nick-booth@live.com>
2021-12-02 08:22:21 +0000

    Day 2 Part 2

--- advent21/Day2/Day2.cs
+++ advent21/Day2/Day2.cs
@@ -6,7 +6,7 @@ public class Day2 : DayBase
     {
         var input = GetStringAndNumberInput(".\\Day2\\puzzleinput.txt"); //Puzzle Input
 
-        Console.WriteLine($"Total: {ProcessInputPart1(input)}"); ; // Part 1 
+        Console.WriteLine($"Total: {ProcessInputPart2(input)}"); ; 
         Console.ReadKey();
     }
 
@@ -30,5 +30,28 @@ public class Day2 : DayBase
             }
             return direction;
         }, (direction) => direction.Horizontal * direction.Depth);
+    }
+
+    private int ProcessInputPart2(IEnumerable<(string direction, int distance)> input)
+    {
+        return input.Aggregate(
+            (Depth: 0, Horizontal: 0, Aim: 0),
+            (direction, input) =>
+            {
+                switch (input.direction)
+                {
+                    case "up":
+                        direction.Aim -= input.distance;
+                        break;
+                    case "down":
+                        direction.Aim += input.distance;
+                        break;
+                    case "forward":
+                        direction.Horizontal += input.distance;
+                        direction.Depth += (direction.Aim * input.distance);
+                        break;
+                }
+                return direction;
+            }, (direction) => direction.Horizontal * direction.Depth);
     }
 }
